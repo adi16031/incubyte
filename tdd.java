9a8c3442d972ef3642f625d8a62a7bbed9d44a7b
Aditya Singh <adity.347317@gmail.com>
2021-02-11 23:32:11 +0530

    refractored a bit

--- tdd.java
+++ tdd.java
@@ -1,13 +1,9 @@
-import org.graalvm.compiler.core.common.type.ArithmeticOpTable.BinaryOp.Add;
-
 class StringCalculator{
 
     public static int Add(String numbers){
+
         if(numbers.length() == 0){
             return 0;
-        }
-        else if(numbers.length() == 1){
-            return(Integer.parseInt(numbers));
         }
         else{
             String arr[]= numbers.split(",");

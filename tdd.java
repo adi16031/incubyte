f57773542fddef9895d20574ce43759d36850462
Aditya Singh <adity.347317@gmail.com>
2021-02-11 23:43:56 +0530

    \n delimiter

--- tdd.java
+++ tdd.java
@@ -6,10 +6,12 @@ class StringCalculator{
             return 0;
         }
         else{
-            String arr[]= numbers.split(",");
+            String arr[]= numbers.split("\n");
             int sum = 0;
             for(String t :arr){
-                sum += Integer.parseInt(t);
+                String temp[] = t.split(",");
+                for(String k:temp)
+                sum += Integer.parseInt(k);
             }
             return sum;
         }
@@ -24,5 +26,9 @@ class StringCalculator{
 
         //for more than two numbers
         System.out.println(Add("1,2,3,4,5,5,6,7,8,8,9,2,0,-10"));
+
+        //for new line (\n) delimiter
+        System.out.println(Add("1,2\n3"));
+        System.out.println(Add("1\n2"));
     }
 }

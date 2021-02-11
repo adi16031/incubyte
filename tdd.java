2eeb4fe735f649f4df7e1e3d48ab8b8ebd53eebf
Aditya Singh <adity.347317@gmail.com>
2021-02-12 00:47:42 +0530

    Added support for multiple delimeters

--- tdd.java
+++ tdd.java
@@ -1,20 +1,61 @@
+
 class StringCalculator{
 
     public static int Add(String numbers){
-
+        int sum =0;
         if(numbers.length() == 0){
             return 0;
         }
         else{
-            String arr[]= numbers.split("\n");
-            int sum = 0;
+            int index = getIndexofFirstNumber(numbers);
+            if(index > 0){
+                String delimiters = "";
+                if(index>2)
+                  delimiters = numbers.substring(2, index);
+                  //System.out.println(delimiters);
+                String nums = numbers.substring(index, numbers.length());
+                sum = 0;
+                //String arr[] = nums.split("");
+                int i =0;
+                String temp = "";
+                for(i = 0;i<delimiters.length();i++){
+                    for(int j =0;j<nums.length();j++){
+                        if(nums.charAt(j) != delimiters.charAt(i)){
+                            temp += nums.charAt(j);
+                        }
+
+                    }
+                    nums =  temp;
+                    temp = "";
+                }
+                String p[] = nums.split("");
+                for(int k=0;k<p.length;k++){
+                    sum += Integer.parseInt(p[k]);
+                    //System.out.println(g);
+                }
+                return sum;
+            }
+            else{
+                String arr[]= numbers.split("\n");
+            sum = 0;
             for(String t :arr){
                 String temp[] = t.split(",");
                 for(String k:temp)
                 sum += Integer.parseInt(k);
             }
+            }
+
             return sum;
         }
+    }
+
+    public static int getIndexofFirstNumber(String s){
+        int i =0;
+        for(i=0;i<s.length();i++){
+            if((int)s.charAt(i) > 47 && (int)s.charAt(i) < 58)
+                break;
+        }
+        return i;
     }
 
     public static void main(String args[]){
@@ -30,5 +71,8 @@ class StringCalculator{
         //for new line (\n) delimiter
         System.out.println(Add("1,2\n3"));
         System.out.println(Add("1\n2"));
+
+        //for delimiters case
+        System.out.println(Add("//;\n1;2"));
     }
 }
